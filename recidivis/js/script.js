68b6569946d3a6aa24e6a654c46f4a26ec4f7eae
ян. <crowbeat.47@gmail.com>
2025-02-09 02:01:24 +0500

    Delete recidivis directory

--- recidivis/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.burger__menu').click(function(event) {
-        $('.burger__menu, .header').toggleClass('active');
-        $('body').toggleClass('lock');
-    })    
-})
