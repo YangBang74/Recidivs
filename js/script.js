fa1a8fe285023e715667cc9ff7f64b7934ed9d15
ян. <crowbeat.47@gmail.com>
2025-02-11 17:10:17 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-$(document).ready(function(){
-    $('.burger__menu').click(function(event) {
-        $('.burger__menu, .header').toggleClass('active');
-        $('body').toggleClass('lock');
-    })    
-})
+const tabsBtn = document.querySelectorAll('.products__btn');
+const tabsItems = document.querySelectorAll('.products__tab');
+const burgerMenu = document.querySelector('.burger__menu');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item){
+    item.addEventListener("click", function(){
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("date-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if ( ! currentBtn.classList.contains('active')){
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            tabsItems.forEach(function(item){
+                item.classList.remove('active');
+            });
+
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+        
+    })
+}
+burgerMenu.addEventListener("click", () =>{
+        burgerMenu.classList.toggle("click");
+        header.classList.toggle("click");
+        body.classList.toggle("click");
+    })
